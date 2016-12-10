93cd87bc8917b2c5c876fcf7507dbe4d6faf2b4b
Heloá Meneses <heloa.meneses@gmail.com>
2016-12-10 21:51:33 -0200

    Adding auth reducers

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,32 @@
+import {
+  EMAIL_CHANGED,
+  LOGIN_USER,
+  LOGIN_USER_FAIL,
+  LOGIN_USER_SUCCESS,
+  PASSWORD_CHANGED
+ } from '../actions/types';
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  user: null,
+  error: '',
+  loading: false
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case EMAIL_CHANGED:
+      return { ...state, email: action.payload };
+    case LOGIN_USER:
+      return { ...state, loading: true, error: '' };
+    case LOGIN_USER_SUCCESS:
+      return { ...state, ...INITIAL_STATE, user: action.payload };
+    case LOGIN_USER_FAIL:
+      return { ...state, loading: false, error: 'Authentication failed.', password: '' };
+    case PASSWORD_CHANGED:
+      return { ...state, password: action.payload };
+    default:
+      return state;
+  }
+};
