1b8d95dfc5103edc1d0337fe4783dbbc6416d7f9
Heloá Meneses <heloa.meneses@gmail.com>
2016-12-10 19:14:01 -0200

    Adding Firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAF4L5UKGHVcWXXbarKbBpPPqle77euJmY',
+      authDomain: 'tabarato-152119.firebaseapp.com',
+      databaseURL: 'https://tabarato-152119.firebaseio.com',
+      storageBucket: 'tabarato-152119.appspot.com',
+      messagingSenderId: '307951384130'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View>
