c84f7731178050e91e4209af3f41c85a71d1d2a1
Heloá Meneses <heloa.meneses@gmail.com>
2016-12-11 16:38:53 -0200

    Adding bargainList and bargainCreate scenes

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import { Router, Scene } from 'react-native-router-flux';
+import { Actions, Router, Scene } from 'react-native-router-flux';
+import BargainCreate from './components/BargainCreate';
+import BargainList from './components/BargainList';
 import LoginForm from './components/LoginForm';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
+
+      <Scene key="main">
+        <Scene
+          key="bargainList"
+          component={BargainList}
+          // localization pt_BR
+          title="Ofertas"
+          rightTitle="Adicionar"
+          onRight={() => Actions.bargainCreate()}
+          initial
+        />
+        <Scene
+          component={BargainCreate}
+          key="bargainCreate"
+          // localization pt_BR
+          title="Adicionar oferta"
+        />
+      </Scene> 
+        
       <Scene
-        key='login'
+        key="login"
         component={LoginForm}
-        title='Login' 
+        title="Login" 
       />
+
     </Router>
   );
 };
